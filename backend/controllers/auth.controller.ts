cd902467061673a8799ef020879b2cdfdf2e3630
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-09-24 21:31:19 -0300

    feat: implement zod for auth validation

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,90 +1,77 @@
-import { Request, Response } from "express";
-import User from "../entities/user.entity";
-import jwt, { Secret } from "jsonwebtoken";
-import { getUserFromToken } from "../services/token.service";
-import InvalidToken from "../entities/invalidToken.entity";
+import { Request, Response } from 'express';
+import User from '../entities/user.entity';
+import jwt, { Secret } from 'jsonwebtoken';
+import { getUserFromToken } from '../services/token.service';
+import InvalidToken from '../entities/invalidToken.entity';
 
 export default {
-	
-	async logInHandler(req: Request, res: Response) {
-		console.log("Loggin user in...");
-		const user = await User.objects.findOne({
-			where: {
-				username: req.body.username,
-			},
-			select: {
-				id: true,
-				username: true,
-				password: true,
-			},
-		});
-		if (user && user.password === req.body.password) {
-			console.log(`Correct password for user ${user.username}`);
+  async logInHandler(req: Request, res: Response) {
+    console.log('Loggin user in...');
+    const user = await User.objects.findOne({
+      where: {
+        username: req.body.username,
+      },
+      select: {
+        id: true,
+        username: true,
+        password: true,
+      },
+    });
+    if (user && user.password === req.body.password) {
+      console.log(`Correct password for user ${user.username}`);
 
-			const token = jwt.sign(
-				{
-					username: req.body.username,
-					id: user.id,
-					email: user.email,
-				},
-				process.env.JWT_SECRET as Secret,
-				{ expiresIn: "120h" }
-			);
-			res.status(201).json({ token: token, username: user.username, userId: user.id });
-			return;
-		}
-		console.log("Authentication failed");
-		res.status(404).json({
-			error: "Credenciais inválidas.",
-		});
-	},
+      const token = jwt.sign(
+        {
+          username: req.body.username,
+          id: user.id,
+          email: user.email,
+        },
+        process.env.JWT_SECRET as Secret,
+        { expiresIn: '120h' }
+      );
+      res
+        .status(201)
+        .json({ token: token, username: user.username, userId: user.id });
+      return;
+    }
+    console.log('Authentication failed');
+    res.status(404).json({
+      error: 'Credenciais inválidas.',
+    });
+  },
 
-	async logOutHandler(req: Request, res: Response) {
-		const { token, userId } = req.body;
+  async logOutHandler(req: Request, res: Response) {
+    const { token, userId } = req.body;
 
-		if (!token || !userId) {
-			res.status(404).json({
-				error: "User or Token not found.",
-			});
-			return;
-		}
+    const newInvalidToken = InvalidToken.objects.create({
+      owner: {
+        id: userId,
+      },
+      token,
+    });
 
-		const newInvalidToken = InvalidToken.objects.create({
-			owner: {
-				id: userId,
-			},
-			token,
-		});
+    const savedInvalidToken = await InvalidToken.objects.save(newInvalidToken);
+    if (!savedInvalidToken) {
+      res.status(500).json({
+        error: 'Something went wrong.',
+      });
+      return;
+    }
 
-		const savedInvalidToken = await InvalidToken.objects.save(newInvalidToken);
+    console.log('Token invalidated');
+    console.log(savedInvalidToken);
 
-		if (!savedInvalidToken) {
-			res.status(500).json({
-				error: "Something went wrong.",
-			});
-			return;
-		}
+    res.status(201).json(savedInvalidToken);
+  },
 
-		console.log("Token invalidated");
-		console.log(savedInvalidToken);
-
-		res.status(201).json(savedInvalidToken);
-	},
-
-	async checkToken(req: Request, res: Response) {
-		if (!req.body.token) {
-			console.log("No token");
-			res.sendStatus(400);
-		}
-		console.log("token found");
-		const user = await getUserFromToken(req.body.token);
-		if (!user) {
-			res.sendStatus(404);
-			return;
-		}
-		console.log("Authenticated from token:");
-		console.log(user);
-		res.status(200).json(user);
-	},
+  async checkToken(req: Request, res: Response) {
+    const user = await getUserFromToken(req.body.token);
+    if (!user) {
+      res.sendStatus(404);
+      return;
+    }
+    console.log('Authenticated from token:');
+    console.log(user);
+    res.status(200).json(user);
+  },
 };
-
--- backend/middleware/cors.middleware.ts
+++ backend/middleware/cors.middleware.ts
@@ -1,10 +1,10 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
 const corsHandler = (req: Request, res: Response, next: NextFunction) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 };
 
 export default corsHandler;
